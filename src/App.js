bb9ed4491e498e365f0fdbb2499c14d333e46140
Spandan Mehta <spandan33@gmail.com>
2022-04-26 00:36:08 +0530

    fixed build error

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import { useEffect, Suspense } from 'react';
 import { getChartData } from './features/sankey/sankeySlice';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import Header from './components/Header';
 import Dashboard from './components/Dashboard';
 
 function App() {
-  const isLoading = useSelector(store => store.isLoading);
   const dispatch = useDispatch();
 
   useEffect(() => {
